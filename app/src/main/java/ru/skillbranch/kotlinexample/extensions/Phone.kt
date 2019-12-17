93f93666ede52cca86773d3e5e829732ba6c06a9
Andrii Puhachenko <andrqxa@gmail.com>
2019-12-17 11:04:29 +0200

    Fixed exceptiopn info

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Phone.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Phone.kt
@@ -5,13 +5,17 @@ fun String.normalizePhone(): String? {
     val shortString = this.trim()
     val templatePhone = "\\+\\s*\\d[^\\p{L}]*".toRegex()
     if(shortString.isBlank() || !templatePhone.matches(shortString)){
-        throw IllegalArgumentException("Enter a valid phone " +
-                "number starting with a and containing 11 digits")
+        throw IllegalArgumentException(
+            "Enter a valid phone number starting with a + " +
+                    "and containing 11 digits"
+        )
     }
     val phone = shortString.substring(1).replace("\\D".toRegex(), "")
     if(phone.length != 11){
-        throw IllegalArgumentException("Enter a valid phone " +
-                "number starting with a and containing 11 digits")
+        throw IllegalArgumentException(
+            "Enter a valid phone number starting with a + " +
+                    "and containing 11 digits"
+        )
     }
     return "+$phone"
 }
